b40b6b9e2a3443a6c05312fc5971269269fa9ef0
Phil Johnston <johnstonphilip@gmail.com>
2013-09-27 15:52:17 -0400

    beta1.0.0.5
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/plugin-checker/included-plugins/mp-html-check.php
+++ includes/plugin-checker/included-plugins/mp-html-check.php
@@ -1,18 +1,42 @@
 <?php
 /**
- * Install HTML In Post Plugin
+ * This file contains a function which checks if the MP HTML In post plugin is installed.
  *
+ * @since 1.0.0
+ *
+ * @package    MP Core
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2013, Move Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
  */
-function mp_html_theme_updater() {
-	$args = array(
-		'plugin_name' => 'MP HTML In Post', 
-		'plugin_message' => __('You require the Move Plugins HTML In Post plugin. Install it here.', 'mp_html_addon'),
-		'plugin_slug' => 'mp_html', 
-		'plugin_filename' => 'mp-html.php',
-		'plugin_required' => true,
-		'plugin_download_link' => 'http://repo.moveplugins.com/repo/mp-html-in-post/?downloadfile=true'
-	);
-	$mp_core_theme_updater = new MP_CORE_Plugin_Checker($args);
+ 
+/**
+* Check to make sure the MP HTML In Post Plugin is installed.
+*
+* @since    1.0.0
+* @link     http://moveplugins.com/doc/plugin-checker-class/
+* @return   array $plugins An array of plugins to be installed. This is passed in through the mp_core_check_plugins filter.
+* @return   array $plugins An array of plugins to be installed. This is passed to the mp_core_check_plugins filter. (see link).
+*/
+if (!function_exists('mp_html_in_post_plugin_check')){
+	function mp_html_in_post_plugin_check( $plugins ) {
+		
+		$add_plugins = array(
+			array(
+				'plugin_name' => 'MP HTML In Post',
+				'plugin_message' => __('You require the MP HTML In Post plugin. Install it here.', 'mp_html_addon'),
+				'plugin_filename' => 'mp-html.php',
+				'plugin_download_link' => 'http://moveplugins.com/repo/mp-html-in-post/?downloadfile=true',
+				'plugin_info_link' => 'http://moveplugins.com/plugins/mp-html-in-post',
+				'plugin_group_install' => true,
+				'plugin_required' => true,
+				'plugin_wp_repo' => true,
+			)
+		);
+		
+		return array_merge( $plugins, $add_plugins );
+	}
 }
-add_action( 'admin_init', 'mp_html_theme_updater' );
-
+add_filter( 'mp_core_check_plugins', 'mp_html_in_post_plugin_check' );
--- includes/updater/mp-html-addon-update.php
+++ includes/updater/mp-html-addon-update.php
@@ -1,19 +1,35 @@
 <?php
 /**
- * Check for updates for this Plugin
+ * This file contains the function keeps the MP HTML In Post Addon plugin up to date.
  *
+ * @since 1.0.0
+ *
+ * @package    MP HTML In Post Addon
+ * @subpackage Functions
+ *
+ * @copyright  Copyright (c) 2013, Move Plugins
+ * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * @author     Philip Johnston
  */
- if (!function_exists('mp_html_in_post_addon_update')){
-	function mp_html_in_post_addon_update() {
+ 
+/**
+ * Check for updates for the MP HTML In Post Addon Plugin by creating a new instance of the MP_CORE_Plugin_Updater class.
+ *
+ * @access   public
+ * @since    1.0.0
+ * @return   void
+ */
+ if (!function_exists('mp_html_addon_update')){
+	function mp_html_addon_update() {
 		$args = array(
-			'software_name' => 'MP HTML In Post Addon', //<- The exact name of this Plugin. Make sure it matches the title in your mp_repo, edd, and the WP.org repo
-			'software_api_url' => 'http://repo.moveplugins.com',//The URL where EDD and mp_repo are installed and checked
+			'software_name' => 'MP HTML Addon', //<- The exact name of this Plugin. Make sure it matches the title in your mp_repo, edd, and the WP.org repo
+			'software_api_url' => 'http://moveplugins.com',//The URL where EDD and mp_repo are installed and checked
 			'software_filename' => 'mp-html-addon.php',
 			'software_licensed' => false, //<-Boolean
 		);
 		
 		//Since this is a plugin, call the Plugin Updater class
-		$mp_html_in_post_addon_plugin_updater = new MP_CORE_Plugin_Updater($args);
+		$mp_html_addon_update_plugin_updater = new MP_CORE_Plugin_Updater($args);
 	}
  }
-add_action( 'init', 'mp_html_in_post_addon_update' );
+add_action( 'init', 'mp_html_addon_update' );
